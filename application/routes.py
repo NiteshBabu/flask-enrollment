766f4e57050afe6ff4034feb7b3552aadf787c72
Nitesh Babu <NiteshBabu@users.noreply.github.com>
2021-04-15 12:16:39 +0530

    Included models for json data

--- application/routes.py
+++ application/routes.py
@@ -1,12 +1,12 @@
 from . import app
 from flask import render_template
-
+import json
 
 # creating routes
 @app.route('/')
 @app.route('/index')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', login=True)
 
 @app.route('/login')
 def login():
@@ -18,6 +18,15 @@ def register():
   return render_template('register.html')
 
 
-@app.route('/classes')
-def classes():
-  return render_template('courses.html')
+@app.route('/courses')
+def courses():
+  courseData = None
+  term = 6
+  with open('application/models/courses.json', 'r') as f:
+    courseData = json.load(f)
+  return render_template('courses.html', courseData=courseData, term=term)
+
+
+@app.route('/enrollment')
+def enrollment():
+  return render_template('enrollment.html')
